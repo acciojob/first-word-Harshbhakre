7022b9aa27478aed4d9a688ca6f886dc27055749
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-17 11:30:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
-  // your code here
+	s=s.split(" ")
+	return s[0]?s[0]:""
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s));   
